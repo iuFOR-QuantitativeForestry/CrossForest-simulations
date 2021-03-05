4d076d6ffa1e25a46cc73568ab48df57a2fe09e3
cristobal ordonez <a_cristo@pvs.uva.es>
2021-03-05 13:16:12 +0100

    update scripts

--- generated.plots/sim005.sh
+++ generated.plots/sim005.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## make folders
 # OUT_Dir=/data/cristobal/simanfor/HPC
-OUT_Dir=/scratch/uva_iufor_1/uva_iufor_1_3/sim007
+OUT_Dir=/scratch/uva_iufor_1/uva_iufor_1_3/sim005
 if [ ! -d "$OUT_Dir" ]; then mkdir $OUT_Dir; fi
 folders=("/input" "/scenario" "/output")
 for folder in ${folders[@]}
@@ -10,20 +10,20 @@ do
     if [ ! -d "$i_Dir" ]; then mkdir $i_Dir; fi
 done
 ## permiso de ejecucion para los archivos
-chmod +x ~/scripts/sim007.*
-n=200
+chmod +x ~/scripts/sim005.*
+n=40
 ## make input files
-sbatch ~/scripts/sim007.make.input.hpc.sh $n 5 100
+sbatch ~/scripts/sim005.make.input.hpc.sh $n 10 100
 ## make scenario files
 CurrentDir=$(pwd)
 cd $OUT_Dir/scenario
 i=1 
 while [ $i -le $n ]; do
-	filename=scnr_sp999_${i}_5-100.json
-	sed -e "s/_idnumber_/${i}/g" ~/scripts/sim007.template.json > ${filename}
+	filename=scnr_sp999_${i}_10-100.json
+	sed -e "s/_idnumber_/${i}/g" ~/scripts/sim005.template.json > ${filename}
 	echo $filename
 	((i++))
 done
 cd $CurrentDir
 ## make simulations
-##sbatch ~/scripts/sim007.job.array.5-100.sh 
+##sbatch ~/scripts/sim005.job.array.10-100.sh 
